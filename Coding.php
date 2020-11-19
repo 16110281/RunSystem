e5f5a57adc0b2a893009220b570219ee127d6623
16110281 <39847949+16110281@users.noreply.github.com>
2020-11-19 16:08:40 +0700

    Test

---
+++ Coding.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+</head>
+
+<body>
+    <form action="output.php" method="get">
+        Nama : <input type="text" name="nama"><br>
+        Alamat :<textarea name="alamat" rows="5" cols="40"></textarea><br>
+        Jenis Kelamin :
+        <input type="radio" name="jk" value="male">Male
+        <input type="radio" name="jk" value="female">Female<br>
+        <input type="checkbox" name="wni" value="WNI">WNI<br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
+</html>
---
+++ output.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+</head>
+
+<body>
+    <form>
+        Nama : <?php echo $_GET["nama"]; ?><br>
+        Alamat : <?php echo $_GET["alamat"]; ?><br>
+        Jenis Kelamin : <?php echo $_GET["jk"]; ?><br>
+        Kewarganegaraan : <?php echo $_GET["wni"]; ?><br>
+    </form>
+</body>
+
+</html>
